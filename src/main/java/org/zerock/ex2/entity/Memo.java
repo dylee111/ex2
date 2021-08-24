65f865bdd4f8e9c5d96ead3ad6966cdc673e62b0
dylee <dylee0568@gmail.com>
2021-08-24 22:29:06 +0900

    revise

--- src/main/java/org/zerock/ex2/entity/Memo.java
+++ src/main/java/org/zerock/ex2/entity/Memo.java
@@ -4,19 +4,21 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@Entity
-@Table(name = "tbl_memo")
+@Entity // 해당 클래스가 entity class임을 표시, 한 개 이상의 테이블 생성
+@Table(name = "tbl_memo") // 관계형 데이터 베이스에 생성되는 테이블과 관련 설정
 @ToString
 @Getter
-@Builder
-@AllArgsConstructor
-@NoArgsConstructor
+@Builder             // 생성자 생성
+@AllArgsConstructor  // 모든 멤버변수에 대한 생성자 (@Builder와 함께 사용)
+@NoArgsConstructor   // 기본 생성자 (@Builder와 함께 사용)
 public class Memo {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Id // PK(Primary Key) 관련 설정
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // 시퀀스
   private Long mno;
 
+  //추가적인 field가 필요한 경우
+  // (length = 200) == varchar2(200)
   @Column(length = 200, nullable = false)
   private String memoText;
 
--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTests.java
@@ -13,15 +13,18 @@ public class MemoRepositoryTests {
   @Autowired
   MemoRepository memoRepository;
 
+  // MemoRepository 인터페이스가 실제 객체가 어떤 것인지 확인.
   @Test
   public void testClass() {
     System.out.println("testClass::"+memoRepository.getClass().getName());
   }
+
   @Test
   public void testInsert(){
     IntStream.rangeClosed(1,100).forEach(i -> {
       Memo memo = Memo.builder().memoText("Sample..."+i).build();
       memoRepository.save(memo);
+      // 해당 객체가 존재하면 UPDATE, 없다면 INSERT 실행.
     });
   }
 }
